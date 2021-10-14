9122d7d014181502338b923dc74390620faea12f
Brendan Turley <crabtails@gmail.com>
2021-10-14 15:41:31 -0500

    Climatological tests added

--- R_code/qaqc_automation.R
+++ R_code/qaqc_automation.R
@@ -52,6 +52,7 @@ file_sum <- summary_aquatroll(input1)
 input <- data_extract_aquatroll(input1)
  
 
+### initialize unchecked flag matrix
 flags <- matrix(2,dim(input)[1],dim(input)[2]-4)
 ### global missing data test; if NA
 na_flag <- which(is.na(input))
@@ -88,9 +89,10 @@ if(length(lat_f2)>0){
   flags[lat_f2,ind_lat] <- 4
 }
 
-
 ###--------- Test 4) Gross Range Test
 ### these values are from the Aquatroll specification manual: https://in-situ.com/us/aqua-troll-600-multiparameter-sonde
+###--------- Test 5) Climatological Test
+### these values are from World Ocean Database 2018 User's Manual ver 0.10; appendix 11
 # temperature
 ind <- grep('temperature',names(input),ignore.case=T)
 flags[,ind] <- 1
@@ -103,6 +105,17 @@ if(length(t1_f2)>0){
 }
 if(length(t2_f2)>0){
   flags[t2_f2,ind[2]] <- 4
+}
+### climatology only for water temperature
+ind_dep <- grep('depth',names(input),ignore.case=T)
+z <- input[,ind_dep[1]] ### depth used elsewhere for climatology
+t1_f3 <- which(z<100 & temp[,1]>35 | z<100 & temp[,1]<(-2.10))
+t1_f4 <- which(z>=100 & temp[,1]>30 | z>=100 & temp[,1]<(-2.10))
+if(length(t1_f3)>0){
+  flags[t1_f3,ind] <- 3
+}
+if(length(t1_f4)>0){
+  flags[t1_f4,ind] <- 3
 }
 # conductivity
 ind <- grep('conductivity',names(input),ignore.case=T)
@@ -126,6 +139,15 @@ s_f2 <- which(sal>350 | sal<0)
 if(length(s_f2)>0){
   flags[s_f2,ind] <- 4
 }
+### climatology
+s_f3 <- which(z<50 & sal>40 | z<50 & sal<0)
+s_f4 <- which(z>=50 & sal>40 | z>=50 & sal<20)
+if(length(s_f3)>0){
+  flags[s_f3,ind] <- 3
+}
+if(length(s_f4)>0){
+  flags[s_f4,ind] <- 3
+}
 # chlorophyll
 ind <- grep('chlorophyll',names(input),ignore.case=T)
 flags[,ind] <- 1
@@ -148,12 +170,152 @@ o_f2 <- which(rdo>50 | rdo<0)
 if(length(o_f2)>0){
   flags[o_f2,ind] <- 4
 }
+### climatology
+o_f3 <- which(z<50 & rdo>436*1.025/31.26 | z<50 & rdo<0)
+o_f4 <- which(z>=50 & rdo>392*1.025/31.26 | z>=50 & rdo<0)
+if(length(o_f3)>0){
+  flags[o_f3,ind] <- 3
+}
+if(length(o_f4)>0){
+  flags[o_f4,ind] <- 3
+}
 
 
 
-###--------- aquatrol quality control
+###--------- aquatroll quality control
 qaqc_aquatroll <- function (input # input file is the output data.frame from data_extract_aquatroll function
 )
 {
+  ### initialize unchecked flag matrix
+  flags <- matrix(2,dim(input)[1],dim(input)[2]-4)
+  ### global missing data test; if NA
+  na_flag <- which(is.na(input))
+  if(length(na_flag)>0){
+    flags[na_flag] <- 9
+  }
+  ### column names
+  flags <- data.frame(flags)
+  names(flags) <- names(input)[1:(ncol(input)-4)]
+  ###--------- Test 1) Timing/Gap Test
+  ind <- grep('date',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  time_stamp <- ymd_hms(input[,ind])
+  dt <- make_difftime(diff(time_stamp),units='seconds')
+  n_flags <- which(abs(dt)>1)
+  if(length(n_flags)>0){
+    flags[n_flags,ind] <- 4
+  }
   
+  ###--------- Test 3) Location Test
+  ind_lon <- grep('longitude',names(input),ignore.case=T)
+  ind_lat <- grep('latitude',names(input),ignore.case=T)
+  flags[,ind_lon] <- 1
+  flags[,ind_lat] <- 1
+  lon <- input[,ind_lon]
+  lat <- input[,ind_lat]
+  ### impossible locations
+  lon_f2 <- which(abs(lon)>180)
+  lat_f2 <- which(abs(lat)>90)
+  if(length(lon_f2)>0){
+    flags[lon_f2,ind_lon] <- 4
+  }
+  if(length(lat_f2)>0){
+    flags[lat_f2,ind_lat] <- 4
+  }
+  
+  ###--------- Test 4) Gross Range Test
+  ### these values are from the Aquatroll specification manual: https://in-situ.com/us/aqua-troll-600-multiparameter-sonde
+  ###--------- Test 5) Climatological Test
+  ### these values are from World Ocean Database 2018 User's Manual ver 0.10; appendix 11
+  # temperature
+  ind <- grep('temperature',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  temp <- input[,ind]
+  ### out of range
+  t1_f2 <- which(temp[,1]>50 | temp[,1]<(-5))
+  t2_f2 <- which(temp[,2]>50 | temp[,2]<(-5))
+  if(length(t1_f2)>0){
+    flags[t1_f2,ind[1]] <- 4
+  }
+  if(length(t2_f2)>0){
+    flags[t2_f2,ind[2]] <- 4
+  }
+  ### climatology only for water temperature
+  ind_dep <- grep('depth',names(input),ignore.case=T)
+  z <- input[,ind_dep[1]] ### depth used elsewhere for climatology
+  t1_f3 <- which(z<100 & temp[,1]>35 | z<100 & temp[,1]<(-2.10))
+  t1_f4 <- which(z>=100 & temp[,1]>30 | z>=100 & temp[,1]<(-2.10))
+  if(length(t1_f3)>0){
+    flags[t1_f3,ind] <- 3
+  }
+  if(length(t1_f4)>0){
+    flags[t1_f4,ind] <- 3
+  }
+  # conductivity
+  ind <- grep('conductivity',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  cond <- input[,ind]
+  ### out of range
+  c1_f2 <- which(cond[,1]>350000 | cond[,1]<0)
+  c2_f2 <- which(cond[,2]>350000 | cond[,2]<0)
+  if(length(c1_f2)>0){
+    flags[c1_f2,ind[1]] <- 4
+  }
+  if(length(c2_f2)>0){
+    flags[c2_f2,ind[2]] <- 4
+  }
+  # salinity
+  ind <- grep('salinity',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  sal <- input[,ind]
+  ### out of range
+  s_f2 <- which(sal>350 | sal<0)
+  if(length(s_f2)>0){
+    flags[s_f2,ind] <- 4
+  }
+  ### climatology
+  s_f3 <- which(z<50 & sal>40 | z<50 & sal<0)
+  s_f4 <- which(z>=50 & sal>40 | z>=50 & sal<20)
+  if(length(s_f3)>0){
+    flags[s_f3,ind] <- 3
+  }
+  if(length(s_f4)>0){
+    flags[s_f4,ind] <- 3
+  }
+  # chlorophyll
+  ind <- grep('chlorophyll',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  chl <- input[,ind]
+  ### out of range
+  c1_f2 <- which(chl[,1]>100 | chl[,1]<0)
+  c2_f2 <- which(chl[,2]>1000 | chl[,2]<0)
+  if(length(c1_f2)>0){
+    flags[c1_f2,ind[1]] <- 4
+  }
+  if(length(c2_f2)>0){
+    flags[c2_f2,ind[2]] <- 4
+  }
+  # oxygen
+  ind <- grep('rdo concentration',names(input),ignore.case=T)
+  flags[,ind] <- 1
+  rdo <- input[,ind]
+  ### out of range
+  o_f2 <- which(rdo>50 | rdo<0)
+  if(length(o_f2)>0){
+    flags[o_f2,ind] <- 4
+  }
+  ### climatology
+  o_f3 <- which(z<50 & rdo>436*1.025/31.26 | z<50 & rdo<0)
+  o_f4 <- which(z>=50 & rdo>392*1.025/31.26 | z>=50 & rdo<0)
+  if(length(o_f3)>0){
+    flags[o_f3,ind] <- 3
+  }
+  if(length(o_f4)>0){
+    flags[o_f4,ind] <- 3
+  }
+  ### output
+  return(flags)
 }
+
+### test
+qaqc_aquatroll(input)
